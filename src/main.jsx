f2013f1e68475dcfbb38b2cfa85fc5097a4a0bc5
Mahmoud Shirif Abdulazim <mahmoudkn22@icloud.com>
2026-01-22 00:16:54 +0200

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import '../node_modules/flowbite/dist/flowbite.min.js'
-import { BrowserRouter } from 'react-router-dom'
-import ReactDOM from "react-dom/client";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>,
 )
